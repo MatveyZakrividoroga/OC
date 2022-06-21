9baaaab20d8854cae6948245af2562e6cfe9b1bd
MatveyZakrividoroga <106272108+MatveyZakrividoroga@users.noreply.github.com>
2022-06-21 15:32:01 +0300

    Add files via upload

---
+++ Lab6/Client.cpp
@@ -0,0 +1,74 @@
+﻿// Client.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+    HANDLE hNamedPipe;
+    hNamedPipe = CreateFile(
+        "\\\\.\\pipe\\demo_pipe", // имя канала
+        GENERIC_READ | GENERIC_WRITE, // читаем и записываем в канал
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        (LPSECURITY_ATTRIBUTES)NULL, // защита по умолчанию
+        OPEN_EXISTING, // открываем существующий канал
+        0, // атрибуты по умолчанию
+        (HANDLE)NULL // дополнительных атрибутов нет
+    );
+    
+    cout << "Enter N,M\n";
+    int M, N;
+    cin >> N >> M;
+    char h = 'h';
+    DWORD sz;
+    int n;
+    ReadFile(hNamedPipe, &n, sizeof(n), &sz, (LPOVERLAPPED)NULL);
+    short* ar = new short[n];
+    for (int i = 0; i < n; i++) {
+        DWORD s;
+        ReadFile(hNamedPipe, &ar[i], sizeof(ar[i]), &s, NULL);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << ar[i] << " ";
+    }
+    int ind=0;
+    for (int i = 0; i < n; i++) {
+        if (N <= ar[i] && ar[i] <= M) {
+            ind++;
+        }
+    }
+    short* ar1 = new short[ind];
+    ind = 0;
+    for (int i = 0; i < n; i++) {
+        if (N <= ar[i] && ar[i] <= M) {
+            ar1[ind] = ar[i];
+            ind++;
+        }
+
+    }
+    WriteFile(hNamedPipe, &ind, sizeof(ind), &sz, NULL);
+    for (size_t i = 0; i < ind; i++)
+    {
+        DWORD r;
+        WriteFile(hNamedPipe, &ar1[i], sizeof(ar1[i]), &r, NULL);
+
+    }
+    cout << endl;
+    for (size_t i = 0; i < ind; i++) {
+        cout << ar1[i] << " ";
+    }
+    int a;
+    cin >> a;
+    //WriteFile(hNamedPipe, &h, sizeof(h), &sz, (LPOVERLAPPED)NULL);
+    //ReadFile(hNamedPipe, &r, sizeof(r), &sz, (LPOVERLAPPED)NULL);
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Lab6/OC(6).cpp
@@ -0,0 +1,69 @@
+﻿// Client.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+    SECURITY_ATTRIBUTES sa; // атрибуты защиты
+    SECURITY_DESCRIPTOR sd;
+    HANDLE hNamedPipe;
+    sa.nLength = sizeof(sa);
+    sa.bInheritHandle = FALSE; // дескриптор канала ненаследуемый
+    // инициализируем дескриптор защиты
+    InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION);
+    // устанавливаем атрибуты защиты, разрешая доступ всем пользователям
+    SetSecurityDescriptorDacl(&sd, TRUE, NULL, FALSE);
+    sa.lpSecurityDescriptor = &sd;
+    hNamedPipe = CreateNamedPipe("\\\\.\\pipe\\demo_pipe", PIPE_ACCESS_DUPLEX, // читаем из канала и пишем в канал
+        PIPE_TYPE_MESSAGE | PIPE_WAIT, // синхронная передача сообщений
+        1, // максимальное количество экземпляров канала
+        0, // размер выходного буфера по умолчанию
+        0, // размер входного буфера по умолчанию
+        INFINITE, // клиент ждет связь 500 мс
+        &sa // доступ для всех пользователей
+    );
+    char c;
+    cout << "Enter array size:" << "\n";
+    int n;
+    cin >> n;
+    short* arr = new short[n];
+    for (size_t i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+    cout << '\n';
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    if (hNamedPipe == INVALID_HANDLE_VALUE)
+    {
+        cerr << "Creation of the named pipe failed." << endl
+            << "The last error code: " << GetLastError() << endl;
+        cout << "Press any char to finish server: ";
+        cin >> c;
+        return 0;
+    }
+    ConnectNamedPipe(hNamedPipe, (LPOVERLAPPED)NULL);
+ 
+    DWORD sz;
+    WriteFile(hNamedPipe, &n, sizeof(n),&sz, (LPOVERLAPPED)NULL);
+    for (size_t i = 0; i < n; i++)
+    {
+        DWORD size;
+        WriteFile(hNamedPipe, &arr[i], sizeof(arr[i]), &size, NULL);
+    }
+
+    int m;
+    ReadFile(hNamedPipe, &m, sizeof(m), &sz, NULL);
+    short* h = new short[m];
+    for (int i = 0; i < m; i++) {
+        DWORD size;
+        ReadFile(hNamedPipe, &h[i], sizeof(h[i]), &size, NULL);
+    }
+    for (int i = 0; i < m; i++) {
+        cout << h[i] << " ";
+    }
+}
