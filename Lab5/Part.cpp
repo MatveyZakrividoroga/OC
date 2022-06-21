00d6d30608ad3808a2900a16d6518258054bf042
MatveyZakrividoroga <106272108+MatveyZakrividoroga@users.noreply.github.com>
2022-06-21 15:31:08 +0300

    Add files via upload

---
+++ Lab5/Part.cpp
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<Windows.h>
+#include<conio.h>
+#include <set>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    HANDLE end = OpenEvent(EVENT_ALL_ACCESS, FALSE, "end");
+    HANDLE write = (HANDLE)atoi(argv[1]);
+    HANDLE read = (HANDLE)atoi(argv[2]);
+    HANDLE event = OpenEvent(SYNCHRONIZE, FALSE, "1");
+    if (event == NULL) {
+        GetLastError();
+    }
+    WaitForSingleObject(event, INFINITE);
+    cout << "Enter N,M\n";
+    int M, N;
+    cin >> N>>M;
+    int n = atoi(argv[3]);
+    short* ar = new short[n];
+    for (int i = 0; i < atoi(argv[3]); i++) {
+        DWORD s;
+        ReadFile(read, &ar[i], sizeof(ar[i]), &s, NULL);
+    }
+    for (int i = 0; i < atoi(argv[3]); i++) {
+        cout << ar[i]<<" ";
+    }
+    int ind=0;
+    cout << '\n';
+    for (int i = 0; i < n; i++) {
+        if (N <= ar[i] && ar[i] <= M) {
+            ind++;
+        }
+    }
+    short* ar1 = new short[ind];
+    ind = 0;
+    for (int i = 0; i < n; i++) {
+        if (N <= ar[i] && ar[i] <= M) {
+            ar1[ind] = ar[i];
+            ind++;
+        }
+
+    }
+    
+    DWORD sz;
+    WriteFile(write, &ind, sizeof(ind), &sz, NULL);
+
+    for (size_t i = 0; i < ind; i++)
+    {
+        DWORD r;
+        WriteFile(write, &ar1[i], sizeof(ar1[i]), &r, NULL);
+
+    }
+    for (size_t i = 0; i < ind; i++) {
+        cout << ar1[i] << " ";
+    }
+    SetEvent(end);
+    int kostyl;
+    cin >> kostyl;
+}
+
+
---
+++ Lab5/Server.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include<conio.h>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+    
+    HANDLE write, read;
+    HANDLE end = CreateEvent(NULL, FALSE, FALSE, "end");
+    if (end == NULL) {
+        GetLastError();
+    }
+    int n;
+    cout << "Enter array size \n";
+
+    cin >> n;
+    short* arr = new short[n];
+
+    for (size_t i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+    cout << '\n';
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << arr[i]<<" ";
+    }
+    cout << "\n";
+    HANDLE event = CreateEvent(NULL, FALSE, FALSE, "1");
+    SECURITY_ATTRIBUTES sa;
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor = NULL; 
+    sa.bInheritHandle = TRUE;
+    CreatePipe(&read, &write, &sa, 0);
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    char a[80];
+    wsprintf(a, "Part.exe %d %d %d", int(write), int(read), n);
+
+    if (!CreateProcess(NULL, a, NULL, NULL, TRUE,
+        CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        GetLastError();
+    }
+  
+    for (size_t i = 0; i < n; i++)
+    {
+        DWORD size;
+        WriteFile(write, &arr[i], sizeof(arr[i]), &size, NULL);
+    }
+    SetEvent(event);
+ 
+    WaitForSingleObject(end, INFINITE);
+    int m;
+    DWORD sz;
+    ReadFile(read, &m, sizeof(m), &sz, NULL);
+    short* h = new short[m];
+    for (int i = 0; i < m; i++) {
+        DWORD size;
+        ReadFile(read, &h[i], sizeof(h[i]), &size, NULL);
+    }
+    for (int i = 0; i < m; i++) {
+        cout << h[i]<<" ";
+    }
+}
+
+
